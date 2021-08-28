983c345a21f5ef1120ffbeaa8ad7b962653da457
AncoteC <leandro.vidal@ancotec.cl>
2021-08-28 13:58:25 -0400

    hasta ejemplo 3.3 FOR

---
+++ ejercicios/js/ejercicio_bucles.js
@@ -0,0 +1,29 @@
+//Ejemplo 3
+
+//Ejemplo 3.1 (FOR)
+/*let n = prompt("Ingrese n : ")
+let i=0
+let acum = " "
+for (i; i<=n;i++){
+    acum = acum + "-" + i
+}
+console.log(acum)
+
+///Ejemplo 3.2 utilizando variables ya creadas
+n = prompt("Ingrese n : ")
+i=0
+acum = " "
+for (i;i>=(n*-1);i--){
+    acum = acum + ";" + i
+}
+console.log(acum)*/
+
+///Ejemplo 3.3 utilizando variables ya creadas
+let n = prompt("Ingrese n : ")
+let i=0
+let suma = 0
+for (i;i<=n;i++){
+    suma = suma +  i
+}
+console.log(suma)
+
